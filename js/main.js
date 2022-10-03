f6865127c0454b7fa90d3e03cb1fe4fd48dc3e0d
AndreyAh <38111898+AndreyAh@users.noreply.github.com>
2022-10-04 00:11:27 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 
 function getRandomNumbers(from, to) {
   if (Number.isInteger(from) && Number.isInteger(to)) {
-    if (from <= to) {
+    if (from < to) {
       return Math.floor(Math.random() * (to - from + 1)) + from;
     } else {
-      return 'Cтартовое значение не может быть меньше или равно конечному :(. Попробуйте еще раз!';
+      return console.log('Cтартовое значение не может быть меньше или равно конечному :(. Попробуйте еще раз!');
     }
   } else {
     return NaN;
