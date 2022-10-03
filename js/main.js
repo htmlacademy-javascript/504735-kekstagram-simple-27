78b7e57f56bf3631c5d5afd9669db95f85601a56
AndreyAh <38111898+AndreyAh@users.noreply.github.com>
2022-10-03 23:27:48 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,27 @@
 // Генератор числа в диапазоне
 
 function getRandomNumbers(from, to) {
-    if (Number.isInteger(from) && Number.isInteger(to)) {
-        if (from <= to) {
-            return Math.floor(Math.random() * (to - from + 1)) + from;
-        } else {
-            return  'Cтартовое значение не может быть меньше или равно конечному :(. Попробуйте еще раз!';
-        }
-    } else {
-        return NaN;
-    }
+	if (Number.isInteger(from) && Number.isInteger(to)) {
+		if (from <= to) {
+			return Math.floor(Math.random() * (to - from + 1)) + from;
+		} else {
+			return 'Cтартовое значение не может быть меньше или равно конечному :(. Попробуйте еще раз!';
+		}
+	} else {
+		return NaN;
+	}
 }
 
-let randNum = getRandomNumbers(7, 5);
-console.log(randNum);
+const randNum = getRandomNumbers(7, 5);
+
 // Проверка длянны строки
 
 function checkLength(str, minLength, maxLength) {
-    if(str.length <= maxLength && str.length >= minLength) {
-        return true;
-    }
+	if(str.length <= maxLength && str.length >= minLength) {
+		return true;
+	}
 
-    return false;
+	return false;
 }
 
-let strLength = checkLength('cообщение!', 9, 11);
-console.log(strLength);
+const strLength = checkLength('cообщение!', 9, 11);
