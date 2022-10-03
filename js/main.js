2d8529f15ff75614d1352f50849dbc7345ca4ea7
AndreyAh <38111898+AndreyAh@users.noreply.github.com>
2022-10-03 23:32:43 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 // Генератор числа в диапазоне
 
 function getRandomNumbers(from, to) {
-	if (Number.isInteger(from) && Number.isInteger(to)) {
-		if (from <= to) {
-			return Math.floor(Math.random() * (to - from + 1)) + from;
-		} else {
-			return 'Cтартовое значение не может быть меньше или равно конечному :(. Попробуйте еще раз!';
-		}
-	} else {
-		return NaN;
-	}
+  if (Number.isInteger(from) && Number.isInteger(to)) {
+    if (from <= to) {
+      return Math.floor(Math.random() * (to - from + 1)) + from;
+    } else {
+      return 'Cтартовое значение не может быть меньше или равно конечному :(. Попробуйте еще раз!';
+    }
+  } else {
+    return NaN;
+  }
 }
 
-const randNum = getRandomNumbers(7, 5);
+getRandomNumbers(7, 5);
 
 // Проверка длянны строки
 
 function checkLength(str, minLength, maxLength) {
-	if(str.length <= maxLength && str.length >= minLength) {
-		return true;
-	}
+  if(str.length <= maxLength && str.length >= minLength) {
+    return true;
+  }
 
-	return false;
+  return false;
 }
 
-const strLength = checkLength('cообщение!', 9, 11);
+checkLength('cообщение!', 9, 11);
